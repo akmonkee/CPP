494d1a3bddfdaa84ed69c8eb2fa073e9fda4c8f7
Marco Sisto <msisto@e3r5p1.42roma.it>
2025-06-12 13:53:50 +0200

    zombieHorde started

---
+++ CPP01/ex01/Zombie.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msisto <msisto@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/10 12:59:35 by msisto            #+#    #+#             */
+/*   Updated: 2025/06/11 13:20:32 by msisto           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+void	Zombie::announce()
+{
+	std::cout<< this->name << ": BraiiiiiiinnnzzzZ..."<<std::endl;
+}
+
+Zombie::Zombie(std::string zName)
+{
+	this->name = zName;
+}
+
+Zombie::~Zombie()
+{
+	std::cout<< "deconstructed"<<std::endl;
+}
+
