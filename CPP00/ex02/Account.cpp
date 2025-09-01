1fd67f09a0dd013033fe5f16d3962ab7bea8e32b
akmonkee <marcosisto180704@gmail.com>
2025-09-01 15:27:58 +0200

    the code compiles, need to check if its good

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: msisto <msisto@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 11:37:29 by msisto            #+#    #+#             */
-/*   Updated: 2025/09/01 15:16:03 by msisto           ###   ########.fr       */
+/*   Updated: 2025/09/01 15:24:22 by msisto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,12 +94,12 @@ int		Account::checkAmount( void ) const
 void	Account::displayStatus( void ) const
 {
 	_displayTimestamp();
-	std::cout<<"index:"<<this->_accountIndex<<";amount:"<<this->_amount<<";deposits"<<this->_nbDeposits<<";withdrawals:"<<this->_nbWithdrawals<<std::endl;
+	std::cout<<"index:"<<this->_accountIndex<<";amount:"<<this->_amount<<";deposits:"<<this->_nbDeposits<<";withdrawals:"<<this->_nbWithdrawals<<std::endl;
 }
 
 void Account::_displayTimestamp(void)
 {
-	std::time_t now = std::time(nullptr);
+	std::time_t now = std::time(0);
 	std::tm* ltm = std::localtime(&now);
 	char buffer[20];
 
@@ -107,4 +107,7 @@ void Account::_displayTimestamp(void)
 	std::cout << buffer;
 }
 
+Account::~Account( void )
+{
 
+}
