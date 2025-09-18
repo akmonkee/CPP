bc8952817bbf793bdc291f81fa3c128844ec75be
akmonkee <marcosisto180704@gmail.com>
2025-09-18 14:11:32 +0200

    ex01 improvements

--- CPP02/ex01/Fixed.cpp
+++ CPP02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: msisto <msisto@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 13:49:26 by msisto            #+#    #+#             */
-/*   Updated: 2025/09/18 11:32:16 by msisto           ###   ########.fr       */
+/*   Updated: 2025/09/18 14:07:44 by msisto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,16 @@ Fixed::Fixed()
 	this->value = 0;
 }
 
-Fixed& Fixed::FixedInt(int num)
+Fixed::Fixed(int num)
 {
 	std::cout<<"Int constructor called\n";
-	this->valueInt = num;
-	return *this;
+	this->value = num << fBits;
 }
 
-Fixed& Fixed::FixedFloat(int num)
+Fixed::Fixed(float num)
 {
 	std::cout<<"Float constructor called\n";
-	this->valueFloat = num;
-	return *this;
+	value = num * (1 << fBits);
 }
 
 Fixed::Fixed(const Fixed& val)
@@ -45,6 +43,12 @@ Fixed& Fixed::operator=(const Fixed& val)
 	return *this;
 }
 
+std::ostream& operator<<(std::ostream& os, const Fixed& obj)
+{
+	os<<obj.toFloat();
+	return os;
+}
+
 int Fixed::getRawBits() const
 {
 	std::cout<<"getRawBits member function called\n";
@@ -55,6 +59,15 @@ void Fixed::setRawBits(int const raw)
 {
 	std::cout<<"setRawBits member function called\n";
 	this->value = raw;
+}
+
+int	Fixed::toInt( void ) const
+{
+	return static_cast<int>(this->value) / (1 << fBits);
+}
+float	Fixed::toFloat( void ) const
+{
+	return static_cast<float>(this->value) / (1 << fBits);
 }
 
 Fixed::~Fixed()
--- CPP02/ex01/Fixed.hpp
+++ CPP02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: msisto <msisto@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 13:49:01 by msisto            #+#    #+#             */
-/*   Updated: 2025/09/18 11:33:12 by msisto           ###   ########.fr       */
+/*   Updated: 2025/09/18 13:42:26 by msisto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,18 @@
 class	Fixed
 {
 	private:
-		int	value;
-		int	intValue;
-		float	floatValue;
+		int		value;
 		static const int	fBits = 8;
 	public:
 		Fixed();
-		Fixed& FixedInt();
-		Fixed& FixedFloat();
+		Fixed(int num);
+		Fixed(float num);
 		Fixed(const Fixed& val);
 		Fixed& operator=(const Fixed& val);
+		friend std::ostream& operator<<(std::ostream& os, const Fixed& obj);
 		int getRawBits() const;
 		void setRawBits(int const raw);
-		//int toInt( void ) const;
-		//float toFloat( void ) const;
+		int toInt( void ) const;
+		float toFloat( void ) const;
 		~Fixed();
 };
