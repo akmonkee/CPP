9f556efa24b92f0b34079a147b792177e235ae2f
Marco Sisto <msisto@e3r10p5.42roma.it>
2025-10-30 11:13:55 +0100

    added back files I needed to turn in

--- CPP01/ex01/zombieHorde.cpp
+++ CPP01/ex01/zombieHorde.cpp
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msisto <msisto@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/10 12:59:35 by msisto            #+#    #+#             */
-/*   Updated: 2025/10/30 11:13:09 by msisto           ###   ########.fr       */
+/*   Created: 2025/10/30 11:12:35 by msisto            #+#    #+#             */
+/*   Updated: 2025/10/30 11:13:13 by msisto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-void	Zombie::announce()
+Zombie	*zombieHorde(int N, std::string Name)
 {
-	std::cout<< this->name << ": BraiiiiiiinnnzzzZ..."<<std::endl;
-}
-
-void	Zombie::setName(std::string &zName)
-{
-	this->name = zName;
-}
-
-Zombie::~Zombie()
-{
-	std::cout<< "deconstructed"<<std::endl;
+	Zombie *zombieArray;
+	zombieArray = new Zombie[N];
+	for(int i = 0; i < N; i++)
+		zombieArray[i].setName(Name);
+	return (zombieArray);
 }
