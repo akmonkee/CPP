f3e94e749300556b4337a049633093586909ab10
Marco Sisto <msisto@e3r10p5.42roma.it>
2025-11-04 12:13:26 +0100

    fix

--- CPP04/ex01/main.cpp
+++ CPP04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: msisto <msisto@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 11:52:48 by msisto            #+#    #+#             */
-/*   Updated: 2025/11/03 13:16:35 by msisto           ###   ########.fr       */
+/*   Updated: 2025/11/04 12:11:17 by msisto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,15 @@
 
 int main()
 {
+
+	const Animal* j = new Dog();
+	const Animal* k = new Cat();
+
+	delete j;
+	delete k;
+
+	std::cout<<"----------------------------\n";
+
 	int	i = 0;
 	Animal* Animals[] = {new Dog, new Cat, new Dog, new Cat, new Dog, new Cat};
 
